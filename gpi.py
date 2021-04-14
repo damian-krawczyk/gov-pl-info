28e44fae6697a38ec489a8ada15b0ecbf1d5c084
Damian Krawczyk <damian@krawczyk.in>
2021-04-14 22:57:22 +0200

    english translation get back

--- gpi.py
+++ gpi.py
@@ -67,7 +67,7 @@ def send_message(article, feed_name, article_type, channel, test_channel, token)
 
     if 'warning' in article:
         info = article['warning']
-        info_en = info
+        info_en = gs.translate(info,'en')
         message = f"{feed_name}\n{info}\n{info_en}"
         print(message)
         bot.send_message(test_channel,text=message, parse_mode='Markdown')
@@ -77,7 +77,7 @@ def send_message(article, feed_name, article_type, channel, test_channel, token)
         print(f'date: {date}')
         title = article['title']
         print(f'title: {title}')
-        title_en = title
+        title_en = gs.translate(title,'en')
         print(f'title en: {title_en}')
         url = article['url']
         print(f'url: {url}')
@@ -85,7 +85,7 @@ def send_message(article, feed_name, article_type, channel, test_channel, token)
         intro = article['intro']
         print(f'intro: {intro}')
         if intro:
-            intro_en = intro
+            intro_en = gs.translate(intro,'en')
             print(f'intro en: {intro_en}')
             message = f"`{date}` - {article_type}\n**PL:** {title}\n\n{intro}\n---\n**EN:** {title_en}\n\n{intro_en}\n---\n[Szczegóły / Details]({url})"
         else:
